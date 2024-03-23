bc0b17624e597a63adcd8b44e0cef707811f6a42
Kavalskiy <vanyamalisheff@yandex.ru>
2024-03-23 21:29:01 +0300

    Changed urls

--- MeetUp/settings.py
+++ MeetUp/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t55hfa!ujt$5p#d&&t@qrm)#9w1o=x-u44@ypr7uz=-+3wsy0m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
