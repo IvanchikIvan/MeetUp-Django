e96c6f154dbaab25599d054b37fa18a68517f4d9
Kavalskiy <vanyamalisheff@yandex.ru>
2024-03-23 22:00:53 +0300

    Added creation meet page

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('', views.index),
+    path("", views.index),
+    path("meet-creation", views.create_meet),
 ]
+
--- app/views.py
+++ app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
+
+def create_meet(request):
+    return render(request, 'meet.html')
